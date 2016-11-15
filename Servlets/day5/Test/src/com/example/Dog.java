00c0c6931a7ce78045516a920db1fdea727af81a
Heebeck Lee <lhbv1421@hanmail.net>
2016-11-16 00:25:38 +0900

    Add listner example source

---
+++ Servlets/day5/Test/src/com/example/Dog.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Dog {
+	private String breed;
+	
+	public Dog(String breed) {
+		this.breed = breed;
+	}
+	
+	public String getBreed() {
+		return breed;
+	}
+}
---
+++ Servlets/day5/Test/src/com/example/MyServletContextListener.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import javax.servlet.*;
+
+public class MyServletContextListener implements ServletContextListener {
+	@Override
+	public void contextInitialized(ServletContextEvent event) {
+		ServletContext sc = event.getServletContext();
+		String dogBreed = sc.getInitParameter("breed");
+		Dog d = new Dog(dogBreed);
+		sc.setAttribute("dog", d);
+	}
+	
+	@Override
+	public void contextDestroyed(ServletContextEvent event) {
+		// 여기서 할 일은 없다.
+	}
+}
